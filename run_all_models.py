53a25f978bdc66a96845df636dc04a8197a356b1
Sungjae Cho <sungjae.cho.1118@gmail.com>
2021-07-27 19:18:02 +0900

    Receiving execution params, printing running info, and training models consecutively with the new naming rule.

--- run_all_models.py
+++ run_all_models.py
@@ -1,3 +1,11 @@
+'''
+How to run:
+python run_all_models.py <GPU_ID> <project_name> <n_networks> <start_model_index>
+e.g.
+python run_all_models.py 1 STSP-EIRNN_DMS_20 10 0
+python run_all_models.py 2 STSP-EIRNN_DMS_20 10 10
+'''
+
 import numpy as np
 from parameters import *
 import model
@@ -13,9 +21,17 @@ def try_model(gpu_id, run_name, project_name):
 
 try:
     gpu_id = sys.argv[1]
+    project_name = sys.argv[2]
+    n_networks = int(sys.argv[3])
+    start_model_index = int(sys.argv[4])
     print('Selecting GPU ', gpu_id)
+    print('Project name: ', project_name)
+    print('N networks to train: ', n_networks)
+    print('Model index to start: ', start_model_index)
 except:
     gpu_id = None
+    print('python run_all_models.py <GPU_ID> <project_name> <n_networks> <start_model_index>')
+    exit()
 
 
 update_parameters({ 'simulation_reps'       : 0,
@@ -33,7 +49,8 @@ update_parameters({ 'simulation_reps'       : 0,
 task_list = ['DMS']
 
 for task in task_list:
-    for n in range(20):
-        save_fn = task + str(n) + '.pkl'
+    for n in range(start_model_index, start_model_index + n_networks):
+        run_name = '{}-run-{}'.format(task, str(n))
+        save_fn = run_name + '.pkl'
         update_parameters({'trial_type': task, 'save_fn': save_fn})
-        try_model(gpu_id)
+        try_model(gpu_id, run_name, project_name)
